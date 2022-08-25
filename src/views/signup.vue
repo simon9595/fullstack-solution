dad29f14d72502d2bb031a6187c16e6d4725e0e3
szymon <szymon1995@gmx.com>
2022-08-26 01:30:14 +0200

    initial commit

---
+++ src/views/signup.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="signup">
+    <img id="logo-left" src="../assets/icon-left-font.png" alt="groupomania logo">
+    <h1>Sign up</h1>
+    <form>
+      <label for="email">E-mail address</label><br>
+      <input type="email" id="email"><br>
+      <label for="username">Username</label><br>
+      <input type="text" id="username"><br>
+      <label for="password">Password</label><br>
+      <input type="password" if="password"><br>
+      <input type="submit" value="Sign up">
+    </form>
+  </div>
+</template>
+
+<style>
+#logo-left {
+  height: 400px;
+  width: 400px;
+}
+</style>
