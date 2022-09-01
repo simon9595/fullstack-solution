a62d659779913cd23cefc84f170a41e47fbd53f0
szymon <szymon1995@gmx.com>
2022-09-01 22:32:15 +0200

    code revision and hyperlink

--- src/views/login.vue
+++ src/views/login.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="login">
-    <img id="logo-top" src="../assets/icon-above-font.png" alt="groupomania logo">
+    <img id="logo-top" src="../assets/icon-above-font.png" alt="groupomania logo" />
     <h1>Login</h1>
     <form>
-      <label for="email">E-mail:</label><br>
-      <input type="text" v-model="email" name="e-mail"><br>
-      <label for="password">Password</label><br>
-      <input type="password" v-model="password" name="password"><br>
-      <input v-on:click="login()" value="Log in" type="submit">
+      <label for="email">E-mail:</label><br />
+      <input type="text" v-model="email" name="e-mail" required /><br />
+      <label for="password">Password</label><br />
+      <input type="password" v-model="password" name="password" required /><br />
+      <input v-on:click="login()" value="Log in" type="submit" />
     </form>
-    <p>Don't have an account? <router-link to='/signup'>Sign up</router-link></p>
+    <p>Don't have an account? <router-link to="/signup">Sign up</router-link></p>
   </div>
 </template>
 
@@ -18,26 +18,29 @@
   width: 400px;
   height: 400px;
 }
+input {
+  margin: 0.2em;
+}
 </style>
 
 <script>
-import axios from 'axios'
- export default {
+import axios from "axios";
+export default {
   name: "App",
-  data(){
-    return{
-      email:'',
-      password:'',
-    }
+  data() {
+    return {
+      email: null,
+      password: null,
+    };
   },
-  methods:{
-    async login(){
-      let result = await axios.post("http://localhost:3000/api/user/login",{
+  methods: {
+    async login() {
+      let result = await axios.post("http://localhost:3000/api/user/login", {
         email: this.email,
-        password: this.password
-      })
-      console.warn(result)
-    }
-  }
- }
+        password: this.password,
+      });
+      console.warn(result);
+    },
+  },
+};
 </script>
--- src/views/signup.vue
+++ src/views/signup.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="signup">
-    <img id="logo-left" src="../assets/icon-left-font.png" alt="groupomania logo">
+    <img id="logo-left" src="../assets/icon-left-font.png" alt="groupomania logo" />
     <h1>Sign up</h1>
     <form>
-      <label for="email">E-mail address</label><br>
-      <input type="email" v-model="email" required><br>
-      <label for="username">Username</label><br>
-      <input type="text" v-model="username" required><br>
-      <label for="password">Password</label><br>
-      <input type="password" v-model="password" required><br>
-      <input v-on:click="addUser()" class="submit" type="submit" value="Sign up">
+      <label for="email">E-mail address</label><br />
+      <input type="email" v-model="email" required /><br />
+      <label for="username">Username</label><br />
+      <input type="text" v-model="username" required /><br />
+      <label for="password">Password</label><br />
+      <input type="password" v-model="password" required /><br />
+      <input v-on:click="addUser()" class="submit" type="submit" value="Sign up" />
     </form>
+    <p>
+      Already have an account? <router-link to="/">Return to log in page</router-link>
+    </p>
   </div>
 </template>
 
@@ -22,25 +25,25 @@
 </style>
 
 <script>
-import axios from 'axios'
+import axios from "axios";
 export default {
   name: "App",
-  data(){
-    return{
-      email:'',
-      username:'',
-      password:'',
-    }
+  data() {
+    return {
+      email: null,
+      username: null,
+      password: null,
+    };
   },
-  methods:{
-    async addUser(){
-      let result = await axios.post("http://localhost:3000/api/user/signup",{
+  methods: {
+    async addUser() {
+      let result = await axios.post("http://localhost:3000/api/user/signup", {
         username: this.username,
-        email:this.email,
-        password: this.password
-      })
-      console.warn(result)
-    }
-  }
-}
+        email: this.email,
+        password: this.password,
+      });
+      console.warn(result);
+    },
+  },
+};
 </script>
