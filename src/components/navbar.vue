0b73a189db675409beb431a841fa3f916c8cfdcb
szymon <szymon1995@gmx.com>
2022-10-18 01:24:05 +0200

    navbar logo adjustment

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -1,7 +1,7 @@
 <template>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container-fluid">
-    <img class="navbar-brand" id="logo" src="../assets/icon-left-font.png">
+    <img class="navbar-brand" id="logo" src="../assets/navbar-logo.png">
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
